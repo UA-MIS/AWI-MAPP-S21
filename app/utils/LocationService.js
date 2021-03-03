e8cbdedd7b13ce5e4b7d2bcf4d1383bfdfdcd3fd
Annie Meyer-Rasmussen <>
2021-03-03 11:53:35 -0600

    added uselocation file and locationservice

--- app/utils/LocationService.js
+++ app/utils/LocationService.js
@@ -1,10 +1,14 @@
-
+import useLocation from "../hooks/useLocation";
 
 // Returns lat and lon of current location as numbers
 
-export default function LocationService () {
-    var lat = 33.1254;
-    var lon = -87.3248;
+export default function LocationService() {
+  const userLocation = useLocation();
+  console.log(JSON.stringify(userLocation.latitude));
+  // var lat = userLocation.latitude;
+  var lat = 30;
+  // var lon = userLocation.longitude;
+  var lon = -80;
 
-    return { lat, lon };
+  return { lat, lon };
 }
