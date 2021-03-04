e79bfc9eb6c4675e37fb7499af242924b4403010
Annie Meyer-Rasmussen <>
2021-03-04 10:59:58 -0600

    location works if user allows always

--- app/screens/WeatherScreen.js
+++ app/screens/WeatherScreen.js
@@ -1,18 +1,17 @@
-import React, {Component, useEffect, useState} from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { Component, useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-import MapScreen from './MapScreen';
-import AppButton from '../components/AppButton';
-import Screen from '../components/Screen';
-import WeatherDetails from '../components/WeatherDetails';
-import { API_KEY } from '../utils/WeatherApiKey';
-import LocationService from '../utils/LocationService';
-import AddressService from '../utils/AddressService';
+import MapScreen from "./MapScreen";
+import AppButton from "../components/AppButton";
+import Screen from "../components/Screen";
+import WeatherDetails from "../components/WeatherDetails";
+import { API_KEY } from "../utils/WeatherApiKey";
+import LocationService from "../utils/LocationService";
+import AddressService from "../utils/AddressService";
 
-
-export default function WeatherScreen({navigation, route}) {
-    //const { latt } = route.params;
-   React.useEffect(() => {
+export default function WeatherScreen({ navigation, route }) {
+  //const { latt } = route.params;
+  React.useEffect(() => {
     if (route.params?.lat) {
       console.log(route.params?.lat);
     }
@@ -22,90 +21,93 @@ export default function WeatherScreen({navigation, route}) {
       console.log(route.params?.long);
     }
   }, [route.params?.long]);
-//   React.useEffect(() => {
-//     if (route.params?.city) {
-//       console.log(route.params?.city);
-//     }
-//   }, [route.params?.city]);
+  //   React.useEffect(() => {
+  //     if (route.params?.city) {
+  //       console.log(route.params?.city);
+  //     }
+  //   }, [route.params?.city]);
 
-    // const fetchWeather = (lat, lon) => {
-    //     console.log("In the fetch Weather");
-    //     useEffect(() => {
-    //         fetch(
-    //             `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
-    //         )
-    //             .then(res => res.json())
-    //             .then(json => {
-    //                 setWeatherState({
-    //                     isLoading: false,
-    //                     temperature: json.main.temp,
-    //                     weatherCondition: json.weather[0].main,
-    //                     error: null
-    //                 })
-    //         }, [])
+  // const fetchWeather = (lat, lon) => {
+  //     console.log("In the fetch Weather");
+  //     useEffect(() => {
+  //         fetch(
+  //             `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+  //         )
+  //             .then(res => res.json())
+  //             .then(json => {
+  //                 setWeatherState({
+  //                     isLoading: false,
+  //                     temperature: json.main.temp,
+  //                     weatherCondition: json.weather[0].main,
+  //                     error: null
+  //                 })
+  //         }, [])
 
-    //     });
-    // }
+  //     });
+  // }
 
-    const fetchWeather = (lat, lon) => {
+  const fetchWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setWeatherState({
+          isLoading: false,
+          temperature: json.main.temp,
+          weatherCondition: json.weather[0].main,
+          error: null,
+        });
+      });
+  };
 
-            fetch(
-                `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`
-            )
-                .then(res => res.json())
-                .then(json => {
-                    setWeatherState({
-                        isLoading: false,
-                        temperature: json.main.temp,
-                        weatherCondition: json.weather[0].main,
-                        error: null
-                    })
-            });
+  const initialWeatherState = {
+    isLoading: true,
+    temperature: 0,
+    weatherCondition: null,
+    error: null,
+  };
+  const [weatherState, setWeatherState] = useState(initialWeatherState);
+
+  let lat = 30;
+  let lon = -87;
+  let coords = LocationService();
+  let thecity = AddressService(30, -87);
+  console.log(coords.coord.latitude);
+  return (
+    <Screen style={styles.container}>
+      {weatherState.isLoading ? (
+        <AppButton
+          title="Load Weather Data"
+          onPress={() => fetchWeather(lat, lon)}
+        />
+      ) : (
+        <WeatherDetails
+          weather={weatherState.weatherCondition}
+          temperature={weatherState.temperature}
+        />
+      )}
+      <Text style={{ margin: 10 }}>Latitude: {route.params?.lon}</Text>
+      <Text style={{ margin: 10 }}>{thecity}</Text>
+      {/* <Text style={{ margin: 10 }}>{latiii}</Text> */}
+
+      <Button
+        onPress={() =>
+          navigation.navigate("MyModal", {
+            screenName: "WeatherScreen",
+          })
         }
-
-
-    const initialWeatherState = {
-        isLoading: true,
-        temperature: 0,
-        weatherCondition: null,
-        error: null
-    }
-    const [weatherState, setWeatherState] = useState(initialWeatherState);
-
-    let { lat, lon } = LocationService();
-   let thecity = AddressService(lat, lon);
-
-
-    return (
-        <Screen style={styles.container}>
-            {weatherState.isLoading ? (
-                <AppButton title="Load Weather Data" onPress={()=> fetchWeather(lat, lon)} />
-
-            ): (
-                <WeatherDetails 
-                    weather = {weatherState.weatherCondition}
-                    temperature = {weatherState.temperature}
-                />
-            )}
-                            <Text style={{ margin: 10 }}>Latitude: {route.params?.lon}</Text>
-                            <Text style={{ margin: 10 }}>{thecity}</Text>
-
-
-               <Button
-        onPress={() => navigation.navigate('MyModal', {
-            screenName: 'WeatherScreen',
-          })}
         title="Open Modal"
       />
-        </Screen>
-    );
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- app/utils/LocationService.js
+++ app/utils/LocationService.js
@@ -1,14 +1,61 @@
-import useLocation from "../hooks/useLocation";
-
-// Returns lat and lon of current location as numbers
+import React, { useState, useEffect } from "react";
+import { Platform, Text, View, StyleSheet } from "react-native";
+import * as Location from "expo-location";
 
 export default function LocationService() {
-  const userLocation = useLocation();
-  console.log(JSON.stringify(userLocation.latitude));
-  // var lat = userLocation.latitude;
-  var lat = 30;
-  // var lon = userLocation.longitude;
-  var lon = -80;
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  useEffect(() => {
+    (async () => {
+      console.log("get location was called");
 
-  return { lat, lon };
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting..";
+  const coord = {
+    altitude: 0,
+    altitudeAccuracy: -1,
+    latitude: 37.785834,
+    accuracy: 5,
+    longitude: -122.406417,
+    heading: -1,
+    speed: -1,
+  };
+  if (errorMsg) {
+    text = errorMsg;
+    alert(errorMsg);
+    let fakeLocation = {
+      coords: {
+        altitude: 0,
+        altitudeAccuracy: -1,
+        latitude: 37.785834,
+        accuracy: 5,
+        longitude: -122.406417,
+        heading: -1,
+        speed: -1,
+      },
+      timestamp: 1614872382359.7021,
+    };
+    const coord = fakeLocation.coords;
+    return { coord };
+  } else if (location) {
+    text = JSON.stringify(location);
+    let lat = JSON.stringify(location.coords.latitude);
+    //console.log(lat);
+    const coord = location.coords;
+    //console.log(coord.latitude);
+    return { coord };
+  } else {
+    return { coord };
+  }
 }
