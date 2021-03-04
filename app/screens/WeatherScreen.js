e6a6063525018a3fc2c641d3e4cb67b08e9f99ce
aprogers2 <aprogers2@crimson.ua.edu>
2021-03-04 15:17:55 -0600

    Completed WeatherScreen/Map functionality

--- app/screens/WeatherScreen.js
+++ app/screens/WeatherScreen.js
@@ -1,13 +1,12 @@
-import React, {Component, useEffect, useState} from 'react';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import MapScreen from './MapScreen';
-import AppButton from '../components/AppButton';
 import Screen from '../components/Screen';
 import WeatherDetails from '../components/WeatherDetails';
 import { API_KEY } from '../utils/WeatherApiKey';
 import LocationService from '../utils/LocationService';
 import AddressService from '../utils/AddressService';
+import { Icon } from '../hooks/useCachedResources';
 
 
 export default function WeatherScreen({ navigation, route }) {
@@ -39,6 +38,44 @@ export default function WeatherScreen({ navigation, route }) {
         })
     };
 
+    // Add refresh and map buttons in header
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <View style={styles.leftHeaderButton}>
+                    <TouchableOpacity onPress={() => fetchWeather(location.latitude, location.longitude)}>
+                        <Icon
+                            name='arrows-cw'
+                            size={23}
+                        />
+                    </TouchableOpacity>
+                </View>
+            ),
+            headerRight: () => (
+                <View style={styles.rightHeaderButton}>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('WeatherMapScreen', { initLocation: location })}
+                    >
+                        <Icon
+                            name='location'
+                            size={23}
+                        />
+                    </TouchableOpacity>
+                </View>
+            )
+        });
+    }, [location]);
+
+    setLocation
+
+    // Watch for updates to location from map
+    useEffect(() => {
+        if (route.params?.location) {
+            setLocation(route.params?.location);
+        }
+    }, [route.params?.location]);
+
+    // Call API
     useEffect(() => {
         fetchWeather(location.latitude, location.longitude);
     }, [location.latitude, location.longitude]);
@@ -52,126 +89,26 @@ export default function WeatherScreen({ navigation, route }) {
                 />
             ) : (
                 <View>
-                    <Text onPress={() => fetchWeather(location.latitude, location.longitude)}>Current temperature: {weatherState.temperature}</Text>
+                    <Text>Current temperature: {weatherState.temperature}</Text>
                     <Text>Datetime: {weatherState.dtCalled}</Text>
                 </View>
             )}
         </View>
     );
-}
+};
 
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    leftHeaderButton: {
+        marginLeft: 30
+    },
+    rightHeaderButton: {
+        marginRight: 30
     }
-})
-
-
-
-
-
-/*
-export default function WeatherScreen({navigation, route}) {
-    //const { latt } = route.params;
-   React.useEffect(() => {
-    if (route.params?.lat) {
-      console.log(route.params?.lat);
-    }
-  }, [route.params?.lat]);
-  React.useEffect(() => {
-    if (route.params?.long) {
-      console.log(route.params?.long);
-    }
-  }, [route.params?.long]);
-//   React.useEffect(() => {
-//     if (route.params?.city) {
-//       console.log(route.params?.city);
-//     }
-//   }, [route.params?.city]);
-
-    // const fetchWeather = (lat, lon) => {
-    //     console.log("In the fetch Weather");
-    //     useEffect(() => {
-    //         fetch(
-    //             `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
-    //         )
-    //             .then(res => res.json())
-    //             .then(json => {
-    //                 setWeatherState({
-    //                     isLoading: false,
-    //                     temperature: json.main.temp,
-    //                     weatherCondition: json.weather[0].main,
-    //                     error: null
-    //                 })
-    //         }, [])
-
-    //     });
-    // }
-
-    const fetchWeather = (lat, lon) => {
-
-            fetch(
-                `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=imperial`
-            )
-                .then(res => res.json())
-                .then(json => {
-                    setWeatherState({
-                        isLoading: false,
-                        temperature: json.main.temp,
-                        weatherCondition: json.weather[0].main,
-                        error: null
-                    })
-            });
-        }
-
-
-    const initialWeatherState = {
-        isLoading: true,
-        temperature: 0,
-        weatherCondition: null,
-        error: null
-    }
-    const [weatherState, setWeatherState] = useState(initialWeatherState);
-
-    let curLocation = LocationService();
-    let thecity = AddressService(curLocation.latitude, curLocation.longitude);
-
-
-    return (
-        <Screen style={styles.container}>
-            {weatherState.isLoading ? (
-                <AppButton title="Load Weather Data" onPress={()=> fetchWeather(lat, lon)} />
-
-            ): (
-                <WeatherDetails 
-                    weather = {weatherState.weatherCondition}
-                    temperature = {weatherState.temperature}
-                />
-            )}
-                            <Text style={{ margin: 10 }}>Latitude: {route.params?.lon}</Text>
-                            <Text style={{ margin: 10 }}>{thecity}</Text>
-
-
-               <Button
-        onPress={() => navigation.navigate('MyModal', {
-            screenName: 'WeatherScreen',
-          })}
-        title="Open Modal"
-      />
-        </Screen>
-    );
-}
-
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-})
-*/
+});
