02bf64064753389a2b10b94a2f7b0852c4cd7cc9
Annie Meyer-Rasmussen <>
2021-03-04 18:04:58 -0600

    added tide stations

--- app/screens/WeatherMapScreen.js
+++ app/screens/WeatherMapScreen.js
@@ -1,79 +1,99 @@
-import React, { useLayoutEffect, useState } from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
-import { HeaderBackButton } from 'react-navigation-stack';
+import React, { useLayoutEffect, useState } from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+import { HeaderBackButton } from "react-navigation-stack";
 
-import Screen from '../components/Screen';
-
+import Screen from "../components/Screen";
+import TideStations from "../assets/TideStations.json";
 
 export default function WeatherMapScreen({ navigation, route }) {
+  const { initLocation } = route.params;
 
-    const { initLocation } = route.params;
+  const handleMapPress = (event) => {
+    setMapState({
+      mapRegion: mapState.mapRegion,
+      locationResult: mapState.locationResult,
+      location: event.nativeEvent.coordinate,
+    });
+  };
 
-    const handleMapPress = (event) => {
-        
-        setMapState({
-            mapRegion: mapState.mapRegion,
-            locationResult: mapState.locationResult,
-            location: event.nativeEvent.coordinate
-        })
-    };
+  const initialMapRegion = {
+    latitude: 33.1254,
+    longitude: -87.3248,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
 
-    const initialMapRegion = { latitude: 33.1254, longitude: -87.3248, latitudeDelta: 0.0922, longitudeDelta: 0.0421 };
+  const initialMapState = {
+    mapRegion: initialMapRegion,
+    locationResult: null,
+    location: initLocation,
+  };
 
-    const initialMapState = {
-        mapRegion: initialMapRegion,
-        locationResult: null,
-        location: initLocation
-    }
+  const [mapState, setMapState] = useState(initialMapState);
 
-    const [mapState, setMapState] = useState(initialMapState);
+  useLayoutEffect(() => {
+    //console.log(mapState.location);
+    navigation.setOptions({
+      headerLeft: () => (
+        <HeaderBackButton
+          onPress={() => {
+            navigation.navigate("WeatherScreen", {
+              location: mapState.location,
+            });
+          }}
+        />
+      ),
+    });
+  }, [mapState.location]);
 
-    useLayoutEffect(() => {
-        //console.log(mapState.location);
-        navigation.setOptions({
-            headerLeft: () => (
-                <HeaderBackButton onPress={() => {
-                    navigation.navigate('WeatherScreen', {
-                        location: mapState.location
-                    });
-                }}/>
-            )
-        });
-    }, [mapState.location])
-    
-    
-    return (
-        <Screen style={styles.container}>
-            <MapView style={styles.map} 
-                region={{
-                    latitude: mapState.location.latitude,
-                    longitude: mapState.location.longitude,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0921
-                }}
-
-                onPress={(event) => {handleMapPress(event)}}
-            >
-                <MapView.Marker
-                    coordinate={mapState.location}
-                    title="Location"
-                    description="Your Current Location"
-                />
-            </MapView>
-        </Screen>
-    );
+  return (
+    <Screen style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={{
+          latitude: mapState.location.latitude,
+          longitude: mapState.location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0921,
+        }}
+        onPress={(event) => {
+          handleMapPress(event);
+        }}
+      >
+        <MapView.Marker
+          coordinate={mapState.location}
+          title="Location"
+          description="Your Current Location"
+        />
+        {TideStations.TideStations.map((markerData) => {
+          var lat = parseFloat(markerData.lat);
+          var long = parseFloat(markerData.long);
+          return (
+            <MapView.Marker
+              key={markerData.stationnumber}
+              coordinate={{
+                latitude: lat,
+                longitude: long,
+              }}
+              title={markerData.Location}
+            />
+          );
+        })}
+      </MapView>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    map:{
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
